4afcb9d0bc52338bc239fb162b939efb484a39c9
Priyanshu Prasad Gupta <priyanshoon.pg@gmail.com>
2025-04-01 22:59:54 +0530

    misc: Tinkering with frontend vuejs

--- frontend/tartalom-client/src/App.vue
+++ frontend/tartalom-client/src/App.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
-    <HelloWorld msg="Hello, World!" />
+    <h1>Hello World</h1>
+    <HelloWorld msg="Hello India" />
 </template>
