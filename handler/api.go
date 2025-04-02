33ec992fdc0e40b7c2addf13cf858134ffcdb15b
Priyanshu Prasad Gupta <priyanshoon.pg@gmail.com>
2025-04-03 02:21:34 +0530

    non essential commit

--- handler/api.go
+++ handler/api.go
@@ -1,7 +1,18 @@
 package handler
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+
+	"tartalom/model"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func Hello(c *fiber.Ctx) error {
+	user, ok := c.Locals("user").(*model.User)
+	if !ok {
+		log.Println("gand mara looo")
+	}
+	log.Println(user.Email)
 	return c.SendString("Api")
 }
