2dc48c65f71cefb78b043a709705b92381e763a6
Priyanshu Prasad Gupta <priyanshoon.pg@gmail.com>
2025-04-04 03:23:37 +0530

    test: Testing setup, fuck idk what am I doing :)

--- tests/blog_test.go
+++
@@ -1,9 +0,0 @@
-package tests
-
-import (
-	"testing"
-)
-
-func TestGetAllBlogsRoute(t *testing.T)         {}
-func TestGetAllBlogsFromServiceID(t *testing.T) {}
-func TestGetServiceIdSpecificBlog(t *testing.T) {}
